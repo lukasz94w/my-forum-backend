882a7d949f1ed4d0b295b020fec265cc43b53beb
lukasz94w <lukasz94w@wp.pl>
2021-09-26 15:03:19 +0200

    Initial commit

---
+++ my-forum/src/test/java/pl/lukasz94w/myforum/MyForumApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukasz94w.myforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
