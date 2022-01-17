d63a251b0aa4bb23e4f5e140a56622dc6d1d0461
lukasz94w <lukasz94w@wp.pl>
2022-01-17 17:31:00 +0100

    API for searching topics by title contains. Some refactoring

--- my-forum-backend/src/main/java/pl/lukasz94w/myforum/repository/TopicRepository.java
+++ my-forum-backend/src/main/java/pl/lukasz94w/myforum/repository/TopicRepository.java
@@ -26,4 +26,6 @@ public interface TopicRepository extends JpaRepository<Topic, Long> {
     List<Topic> findLatestTopicInEachCategory();
 
     Page<Topic> findByUser(User user, Pageable pageable);
+
+    Page<Topic> findByTitleContainsIgnoreCase(String title, Pageable pageable);
 }
